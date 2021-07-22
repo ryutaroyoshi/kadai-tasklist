3391b4aac6a6769d2f162a2e74235e8a2f83abb2
ryutaro <r_yoshikawa@outlook.com>
2021-07-22 20:17:23 +0900

    EditServletのtask_idをセッションスコープに変更

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -30,7 +30,7 @@ public class EditServlet extends HttpServlet {
 
         request.setAttribute("task" , t);
         request.setAttribute("_token", request.getSession().getId());
-        request.setAttribute("task_id", t.getId());
+        request.getSession().setAttribute("task_id", t.getId());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
